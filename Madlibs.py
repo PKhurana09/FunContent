e524192f238ba00908f3e445b1ac997e19d83716
PKhurana09 <s3758609@student.rmit.edu.au>
2022-06-07 16:50:18 +1000

    Madlib

---
+++ Madlibs.py
@@ -0,0 +1,8 @@
+# madlib is aboult filling blanks in a paragraph. We gonna use the concept of string concatination to make this game.
+
+name = input("Name: ")
+profession = input("Profession: ")
+company = input("Company: ")
+dream = input("Dream Job: ")
+
+print(f"Hey my name is {name}. I am working as {profession} at {company}. My dream job is {dream}.")
